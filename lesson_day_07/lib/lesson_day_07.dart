f8e746a5155944a7e941608a37f7f798fc44800d
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-10-03 14:59:47 +0800

    add: lesson day 08

--- lesson_day_07/lib/lesson_day_07.dart
+++ lesson_day_07/lib/lesson_day_07.dart
@@ -1,3 +1,40 @@
-int calculate() {
-  return 6 * 7;
+void main(){
+  print('Lesson Day - 07');
+  print('Dart classes');
+
+  final Human khangai = Human();
+  khangai.name = 'khangaikhuu'; // after my birth, i get my first name
+  khangai.age = 41;
+  khangai.gender = 'male';
+  print(khangai.name);
+  print(khangai.age);
+  print(khangai.gender);
+
+  final Human anna = Human();
+  anna.name = 'Anna';
+  anna.age = 25;
+  anna.gender = 'female';
+  print(anna.name);
+  print(anna.age);
+  print(anna.gender);
+
+  final Human putin = Human();
+  putin.name = 'Putin';
+  putin.age = 70;
+  putin.gender = 'male';
+  print(putin.name);
+  print(putin.age);
+  print(putin.gender);
+
 }
+
+class Human {
+  String name = 'human'; // class property
+  int age = 0;
+  int body = 1;
+  int eyes = 2;
+  int feet = 2;
+  int hands = 2;
+  String gender = 'woman';
+}
+
