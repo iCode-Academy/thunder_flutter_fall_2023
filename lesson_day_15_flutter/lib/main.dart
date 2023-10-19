52d9f49973b068ebf7829c6192790ae9dc9474d1
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-10-19 16:50:08 +0800

    add: lesson day 15 flutter

--- lesson_day_15_flutter/lib/main.dart
+++ lesson_day_15_flutter/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:lesson_day_15_flutter/basic_screen.dart';
 import 'package:lesson_day_15_flutter/immutable_widget.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: ImmutableWidget(),
+    home: BasicScreen(),
   ));
 }
 
