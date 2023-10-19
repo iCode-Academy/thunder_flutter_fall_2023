d57ceaa502ccd8ee28d33ed330de089af38de614
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-10-19 14:34:43 +0800

    add: lesson day 15 immutable widget

---
+++ lesson_day_15_flutter/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:lesson_day_15_flutter/immutable_widget.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ImmutableWidget(),
+  ));
+}
+
