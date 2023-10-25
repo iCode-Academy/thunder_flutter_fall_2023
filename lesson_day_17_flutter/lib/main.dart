2fb5880c125a29283f1e0bcad58d4dc330d33cb4
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-10-25 12:34:53 +0800

    add: lesson day 17

---
+++ lesson_day_17_flutter/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:lesson_day_17_flutter/my_app.dart';
+import 'package:lesson_day_17_flutter/text_field.dart';
+import 'package:lesson_day_17_flutter/text_field_with_controller.dart';
+
+void main() {
+  runApp(MaterialApp(home: TextFieldTwo()));
+}
