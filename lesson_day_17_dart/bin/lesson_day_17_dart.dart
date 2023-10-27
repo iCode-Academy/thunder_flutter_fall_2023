75026527ec438a7305b26f9129f3479e0c49cc7b
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-10-27 12:47:38 +0800

    add: lesson day 18

--- lesson_day_17_dart/bin/lesson_day_17_dart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:lesson_day_17_dart/lesson_day_17_dart.dart' as lesson_day_17_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${lesson_day_17_dart.calculate()}!');
-}
