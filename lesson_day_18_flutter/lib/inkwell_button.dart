0bfe019d41089b5e2b41822a24ccdff002584427
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-10-27 16:02:04 +0800

    add: lesson day 18 and flutter modules

---
+++ lesson_day_18_flutter/lib/inkwell_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class InkwellButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("InkWell Example"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            InkWell(
+              onTap: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('You tapped the box!')),
+                );
+              },
+              child: Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Center(
+                  child: Text(
+                    'Tap Me!',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ thunder_flutter_2023_fall/android/app/src/main/kotlin/com/example/thunder_flutter_2023_fall/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.thunder_flutter_2023_fall
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
