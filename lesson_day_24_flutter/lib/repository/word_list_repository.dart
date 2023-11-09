1f12cfafc950144a48c8dad738f395c7a490e6cb
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-11-09 15:00:56 +0800

    add: word list repostiroy

--- lesson_day_24_flutter/lib/repository/word_list_repository.dart
+++ lesson_day_24_flutter/lib/repository/word_list_repository.dart
@@ -3,10 +3,12 @@ import 'package:lesson_day_24_flutter/models/word_search_model.dart';
 class WordListRepository {
   final List<WordSearchModel> search_words = [
     WordSearchModel(
-        imageURL: 'assets/images/mickey.png', hiddenWord: ['M', 'I', 'C', 'K', 'E', 'Y']),
+        imageURL: 'assets/images/mickey.png',
+        hiddenWord: ['M', 'I', 'C', 'K', 'E', 'Y']),
     WordSearchModel(
-        imageURL: 'assets/images/elsa.png', hiddenWord: ['E','L','S','A']),
+        imageURL: 'assets/images/elsa.png', hiddenWord: ['E', 'L', 'S', 'A']),
     WordSearchModel(
-        imageURL: 'assets/images/totoro.jpeg', hiddenWord: ['T','O', 'T', 'O', 'R', 'O']),
+        imageURL: 'assets/images/totoro.jpeg',
+        hiddenWord: ['T', 'O', 'T', 'O', 'R', 'O']),
   ];
 }
