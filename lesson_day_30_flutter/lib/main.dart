deee30236580d27101d89c1698628990bb04eda9
Khangaikhuu Uvgunkhuu <khangaikhuu@gmail.com>
2023-11-24 13:21:31 +0800

    add: home screen and login

--- lesson_day_30_flutter/lib/main.dart
+++ lesson_day_30_flutter/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lesson_day_30_flutter/firebase_options.dart';
+import 'package:lesson_day_30_flutter/screens/home_screen.dart';
 import 'package:lesson_day_30_flutter/screens/login_screen.dart';
 import 'my_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(MaterialApp(home: LoginScreen()));
+  runApp(MaterialApp(home: HomeScreen()));
 }
